2b67052d4443307efe2a6861bb28a8032012788c
Deepak <deepakkumar750206@gmail.com>
2022-09-24 08:54:38 +0530

    print all permutation of a string

---
+++ Recursion/PrintAllPermutation.java
@@ -0,0 +1,24 @@
+package Recursion;
+
+public class PrintAllPermutation {
+
+    public static void printPerm(String str, String perm){
+        if(str.length() == 0){
+            System.out.println(perm);
+            return;
+        }
+
+        for(int i = 0; i < str.length(); i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1);
+
+            printPerm(newStr,perm+currChar);
+        }
+    }
+    public static void main(String[] args){
+
+        String str = "abc";
+        printPerm(str, "");
+
+    }
+}
