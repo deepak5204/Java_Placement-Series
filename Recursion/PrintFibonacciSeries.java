dab6a17e8c8cfc102f3642819a3bc4cec0f521a5
Deepak <deepakkumar750206@gmail.com>
2022-09-26 21:29:45 +0530

    calculate sum of n fibonacci number

--- Recursion/PrintFibonacciSeries.java
+++ Recursion/PrintFibonacciSeries.java
@@ -11,6 +11,18 @@ public class PrintFibonacciSeries {
         System.out.print(next + " ");
         printFibSeries(n - 1, n2, next);
     }
+
+    //print sum of n number of fibonacci series
+        public static int printFib(int n){
+        if(n == 1 || n == 2){
+            return 1;
+        }
+
+        int fib_nm1 = printFib(n - 1);
+        int fib_nm2 = printFib(n - 2);
+        return fib_nm1 + fib_nm2;
+    }
+
     
     
     public static void main(String[] args){
@@ -19,5 +31,8 @@ public class PrintFibonacciSeries {
         int n2 = 1;
         System.out.print(n1 +" "+ n2 + " ");
         printFibSeries(n-2, n1, n2);
+
+        //print sum of n number of fibonacci series
+        System.out.println("\n sum of " + n +" fibonacci number : " + printFib(n));
     }
 }
