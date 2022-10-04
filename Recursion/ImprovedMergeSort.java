836f084b69a3079dd308133dbe753b2205e3e407
Deepak <deepakkumar750206@gmail.com>
2022-10-04 23:33:34 +0530

    improved merge sort

---
+++ Recursion/ImprovedMergeSort.java
@@ -0,0 +1,53 @@
+package Recursion;
+
+
+public class ImprovedMergeSort {
+    public static void conquer(int[] arr, int si, int mid, int ei){
+
+        int [] mrg = new int[ei - si + 1];
+        int idx1 = si;
+        int idx2 = mid + 1;
+        int x = 0;
+        while(idx1 <= mid && idx2 <= ei){
+            if(arr[idx1] <= arr[idx2]){
+                mrg[x++] = arr[idx1++];
+            } else {
+                mrg[x++] = arr[idx2++];
+            }
+        }
+
+        while(idx1 <= mid){
+            mrg[x++] = arr[idx1++];
+        }
+        
+        while(idx2 <= ei){
+            mrg[x++] = arr[idx2++];
+        }
+
+        for(int i = 0, j = si; i < mrg.length; i++, j++){
+            arr[j] = mrg[i];
+        }
+    }
+
+    public static void divide(int[] arr , int si, int ei){
+        if(si >= ei){
+            return;
+        }
+
+        int mid = si + (ei - si)/2;
+
+        divide(arr, si, mid);
+        divide(arr, mid + 1, ei);
+        conquer(arr, si, mid, ei);
+
+    } 
+    public static void main(String[] args){
+    int [] arr ={3,5,2,8,6,9,4,1};
+    divide(arr,0,arr.length-1);
+    //print sorted array
+    for(int i = 0; i<arr.length; i++){
+        System.out.print(arr[i]+" ");
+    }
+    }
+
+}
