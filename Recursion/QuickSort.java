4f8b97096175a4b543961d23e48cab1ba6d0bf6e
Deepak <deepakkumar750206@gmail.com>
2022-10-09 21:49:25 +0530

    Implement quick sort

---
+++ Recursion/QuickSort.java
@@ -0,0 +1,57 @@
+package Recursion;
+
+public class QuickSort {
+
+    public static int partition(int[] arr, int si, int ei){
+        int pivotElement = arr[si];
+        int smallNumCount =0;
+        for(int i = si; i<= ei; i++){
+            if(arr[i] < pivotElement){
+                smallNumCount++;
+            }
+        }
+
+        int temp = arr[si + smallNumCount];
+        arr[si + smallNumCount] = pivotElement;
+        arr[si] = temp;
+
+        int i = si;
+        int j = ei;
+        while(i < j){
+            if(arr[i] < pivotElement){
+                i++;
+            } else if(arr[j] >= pivotElement){
+                j--;
+            } else {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp; 
+                    i++;
+                    j--;
+                }
+        }
+        return si + smallNumCount;
+
+    }
+
+    public static void qsort(int[] arr, int si, int ei){
+        if(si >= ei){
+            return;
+        }
+
+        int pivotIndex = partition(arr, si, ei);
+        qsort(arr, si, pivotIndex - 1);
+        qsort(arr, pivotIndex + 1, ei);
+
+    }
+    public static void main(String[] args){
+
+        int[] arr = {3,4,5,1,7,8,6};
+
+        qsort(arr, 0, arr.length - 1);
+
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
