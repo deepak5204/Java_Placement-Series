7300ddf8ae395889bf688a3a9b5c9b47b19d38c3
Deepak <deepakkumar750206@gmail.com>
2022-10-14 18:33:05 +0530

    Add scanner class and take array size and array element from user

--- Array/SelectionSort.java
+++ Array/SelectionSort.java
@@ -3,30 +3,32 @@ import java.util.Scanner;
 public class SelectionSort {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[5];
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
         System.out.println("Enter array element");
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
         sc.close();
 
-      for(int p = 0; p < 5; p++){
-          //find minimum element 
-          int mi = p;
-          for(int i = p+1; i < 5; i++){
-             if(arr[p] > arr[i]){
-              mi = i;
+        //selection sort
+      for(int i = 0; i < arr.length - 1; i++){
+          int min = i;
+          for(int j = i+1; j < arr.length; j++){
+             if(arr[j] < arr[min]){
+              min = j;
              }
           }
   
           //swap min value with first value of each iteration
-          int temp = arr[p];
-          arr[p] = arr[mi];
-          arr[mi] = temp;
+          int temp = arr[min];
+          arr[min] = arr[i];
+          arr[i] = temp;
       }
 
     //print array
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < arr.length; i++){
         System.out.print(arr[i]+" ");
     }
     }
