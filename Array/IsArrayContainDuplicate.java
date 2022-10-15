0f42a664ff3232e9f2ade62b91967cfa01f82f68
Deepak <deepakkumar750206@gmail.com>
2022-10-15 21:37:59 +0530

    check array contain duplicate, if arraycontain duplicate return false otherwise retu rn true

---
+++ Array/IsArrayContainDuplicate.java
@@ -0,0 +1,33 @@
+package Array;
+
+import java.util.Scanner;
+
+
+public class IsArrayContainDuplicate {
+
+    //check array contain duplicate or not if array contain duplicate return false otherwise return true
+    public static boolean arrConDuplicate(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of array");
+    int n = sc.nextInt();
+    int [] arr = new int[n];
+    System.out.println("Enter array elements");
+    for(int i = 0; i < n; i++){
+        arr[i] = sc.nextInt();
+    }
+    sc.close();
+
+    boolean res = arrConDuplicate(arr);
+    System.out.println(res);
+   }
+}
