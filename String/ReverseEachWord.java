d1731ae8121d168f85cf37b4e22ebc6aeb02d89b
Deepak <deepakkumar750206@gmail.com>
2022-10-16 16:43:39 +0530

    reverse each word in string not reverse whole string

---
+++ String/ReverseEachWord.java
@@ -0,0 +1,48 @@
+package String;
+
+import java.util.Scanner;
+
+public class ReverseEachWord {
+
+    // reverse string word wise
+    public static String revEachWord(StringBuffer str) {
+        String ans = "";
+        int i = 0;
+        int currentWordStart = 0;
+        for (; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+            //reverse current word
+            int currentWordEnd = i - 1;
+            String reversedWord = "";
+
+                for (int j = currentWordStart; j <= currentWordEnd; j++) {
+                    reversedWord = str.charAt(j) + reversedWord;
+                }
+                ans += reversedWord + " ";
+                currentWordStart = i + 1;
+            }
+        }
+        int currentWordEnd = i - 1;
+            String reversedWord = "";
+
+                for (int j = currentWordStart; j <= currentWordEnd; j++) {
+                    reversedWord = str.charAt(j) + reversedWord;
+                }
+                ans += reversedWord + " ";
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter string ");
+        StringBuffer str = new StringBuffer(s.nextLine());
+        s.close();
+
+        // System.out.println(str);
+
+        
+        System.out.println( revEachWord(str) );
+
+    }
+}
