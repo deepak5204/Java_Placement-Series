25dadd9d2e4daa2b067da8dd8405a43aa1b67892
Deepak <deepakkumar750206@gmail.com>
2022-10-16 23:55:07 +0530

    remove character

--- String/RemoveCharacter.java
+++ String/RemoveCharacter.java
@@ -4,25 +4,34 @@ import java.util.Scanner;
 
 public class RemoveCharacter {
 
-    public static void rmvChar(String str, char ch){
-        String ans = "";
-        for(int i = 0; i < str.length(); i++){
-            if(str.charAt(i) != ch){
-                ans += str.charAt(i);
-            }
-        }
+    
+    // //remove character using extra string
+    // public static void rmvChar(StringBuffer str, char ch){
+    //     String ans = "";
+    //     for(int i = 0; i < str.length(); i++){
+    //         if(str.charAt(i) != ch){
+    //             ans += str.charAt(i);
+    //         }
+    //     }
 
-        System.out.println(ans);
-    }
+    //     System.out.println(ans);
+    // }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         System.out.println("Enter String");
+        // StringBuffer str = new StringBuffer(s.nextLine());
         String str = s.nextLine();
         System.out.println("Enter character");
         char ch = s.next().charAt(0);
         s.close();
 
-        rmvChar(str, ch);
+        // rmvChar(str, ch);
+        // int j = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) != ch){
+                // str = str.replace(j, j+1, str.charAt(i));
+            }
+        }
 
     }
 }
