82a084c5ddc759bfacd08c6b6ecc45d1ae991fe3
Deepak <deepakkumar750206@gmail.com>
2022-10-22 23:02:03 +0530

    count no of zero

---
+++ Recursion/CountZero.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class CountZero {
+    public static int no_of_0(int n, int count){
+        if( n == 0){
+            return count;
+        }
+        if(n%10==0){
+            count++;
+        }
+        n = n/10;
+        return no_of_0(n, count);
+    }
+         
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter any integer");
+        int n = s.nextInt();
+        s.close();
+        int res = no_of_0(n, 0);
+        System.out.println("no of zero = " + res);
+    }
+}
