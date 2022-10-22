36cdf687b43c0a762f1b37befcc3a044e94fb3a8
Deepak <deepakkumar750206@gmail.com>
2022-10-22 21:01:06 +0530

    add scanner class and take no of disk from user

--- Recursion/TowerOfHanoi.java
+++ Recursion/TowerOfHanoi.java
@@ -1,18 +1,27 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class TowerOfHanoi {
-    public static void TOH(int n, String s, String h, String d){
+    public static void TOH(int n, String source, String helper, String dest){
         if(n == 1){
-            System.out.println("Transform from " + s + " to " + d);
+            System.out.println("Transform disk " + n +" from "+ source + " to " + dest);
             return;
         }
 
-        TOH(n-1, s, d, h);
-        System.out.println("Transform from " + s + " to " + d);
-        TOH(n-1, h, s, d);
+        //move n-1 disk form source to helper using destination as a helper
+        TOH(n-1, source, dest, helper);
+
+        System.out.println("Transform disk " + n +" from " + source + " to " + dest);
+
+        //move n-1 disk form helper to destination using source as a helper
+        TOH(n-1, helper, source, dest);
     }
     public static void main(String[] args){
-        int n = 3;
+     Scanner s = new Scanner(System.in);
+     System.out.println("Enter no of disk");
+     int n = s.nextInt();
+     s.close();
     TOH(n, "S", "H", "D");        
 
     }
