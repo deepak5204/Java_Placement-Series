98e040ca85cebdb1323d86280f0be692833badfc
Deepak <deepakkumar750206@gmail.com>
2022-10-23 23:02:23 +0530

    print all substring

--- Recursion/PrintAllSubstring.java
+++ Recursion/PrintAllSubstring.java
@@ -3,20 +3,22 @@ package Recursion;
 import java.util.Scanner;
 
 public class PrintAllSubstring {
-    public static void printSubstring(String str){
-        if(str.length() == 0){
+    public static void printSubstring(String str, int i, String subStr){
+        if(str.length() == i){
+            System.out.println(subStr);
             return;
         }
+        char currChar = str.charAt(i);
 
-        System.out.println(str);
-        printSubstring(str.substring(1));
+        printSubstring(str, i + 1, subStr + currChar);
+        printSubstring(str, i + 1, subStr);
     }
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         System.out.println("Enter any string");
         String str = new String(s.nextLine());
         s.close();
-        printSubstring(str);
+        printSubstring(str, 0, "");
 
     }
 }
