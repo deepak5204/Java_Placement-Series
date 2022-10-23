82b24f77a3141f3943b86d9c2652e34906efd4df
Deepak <deepakkumar750206@gmail.com>
2022-10-23 15:02:15 +0530

    convert string to integer

--- Recursion/StringToIntegerConversion.java
+++ Recursion/StringToIntegerConversion.java
@@ -2,34 +2,41 @@ package Recursion;
 
 import java.util.Scanner;
 
+
 public class StringToIntegerConversion {
 
-    public static int stringToInt(String str){
-       if(str.length() == 1){
-        return str.charAt(0) - '0';
-       }
+    public static int stringToInt(String str) {
+        if (str.length() == 1) {
+            return str.charAt(0) - '0';
+        }
 
-       int recVal = stringToInt(str.substring(1));
+        // int recVal = stringToInt(str.substring(1));
+        // // do small work with first character of a string and store in small value
+        // int smallval = str.charAt(0) - '0';
+        // // find no position
+        // int pos = 1;
+        // for (int i = 0; i < str.length() - 1; i++) {
+        //     pos = pos * 10;
+        // }
+        // // move no into its own position
+        // smallval = pos * smallval;
+        // return smallval + recVal;
 
-    //    do small work with first character of a string  and store in small value
-       int smallval = str.charAt(0) - '0';
+        int y = stringToInt(str.substring(0, str.length()-1) );
 
-       //find no position
-       int pos = 1;
-       for(int i = 0; i < str.length()-1; i++){
-            pos = pos * 10;
-       }
-    //    move no into its own position
-       smallval = pos * smallval; 
-       return smallval + recVal;
+        y = y * 10;
+        System.out.println("y="+y);
+        int x = str.charAt(str.length() - 1) - '0';
+        return x + y;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter any string");
         String str = s.nextLine();
         s.close();
 
-        int res =  stringToInt(str);
+        int res = stringToInt(str);
         System.out.println(res);
     }
 }
