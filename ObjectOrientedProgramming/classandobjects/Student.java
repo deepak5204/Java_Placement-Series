4ba75d4cb2a8321bcad3b3c7f5e56e694919ccb9
Deepak <deepakkumar750206@gmail.com>
2022-10-27 16:13:40 +0530

    understand static keyword

--- ObjectOrientedProgramming/classandobjects/Student.java
+++ ObjectOrientedProgramming/classandobjects/Student.java
@@ -1,25 +1,45 @@
 package ObjectOrientedProgramming.classandobjects;
 
 public class Student {
-    String name;
-    private int roll_no;
+    public String name;
+    final private int roll_no;
     double cgpa;
     // We can not modify it further
-    final double conversionFActor = 0.95;
-    
-    public int getRollNumber(){
-        return roll_no;
+    final static double conversionFActor = 0.95;
+
+    // static variable belong to the class it not belong to every object but it share common functionality with all object .
+    // It does not create seperate memory for every object  
+    static int numStudent;
+
+
+    //constructor
+    public Student(String name){
+        this.name = name;
+        numStudent++;
+        this.roll_no = numStudent;
+    }
+
+    public Student(){
+        name = "xyz";
+        numStudent++;
+        roll_no = numStudent;
     }
     
-    public void setRollNumber(int roll_no){
-        if(roll_no <= 0){
-            return;
-        }
+   
+    
+    // public void setRollNumber(int roll_no){
+    //     if(roll_no <= 0){
+    //         return;
+    //     }
 
-        System.out.println("this = "+ this);
-        // System.out.println("this " +this);
-        // here this is reference to current object on which it is called 
-        this.roll_no = roll_no;
+    //     System.out.println("this = "+ this);
+    //     // System.out.println("this " +this);
+    //     // here this is reference to current object on which it is called 
+    //     this.roll_no = roll_no;
+    // }
+
+    public void print(){
+        System.out.println(name+ " "+ roll_no);
     }
 
 }    
--- ObjectOrientedProgramming/classandobjects/StudentUse.java
+++ ObjectOrientedProgramming/classandobjects/StudentUse.java
@@ -2,14 +2,25 @@ package ObjectOrientedProgramming.classandobjects;
 
 public class StudentUse {
     public static void main(String[] args){
-        Student s1 = new Student();
-        
-        s1.setRollNumber(100);
+        Student s1 = new Student("Tonny");
+        Student s2 = new Student("john");
+
+        System.out.println(s1.numStudent);
+        System.out.println(s2.numStudent);
+        Student s3 = new Student();
+        System.out.println(Student.numStudent);
+        s3.print();
+
+
+
+
+        // s1.print();
+        // s2.print();
+        // s1.setRollNumber(100);
         // System.out.println("s1 = "+ s1);
         // System.out.println(s1.name +" "+ s1.getRollNumber());
-        System.out.println("s1 " + s1);
         
-        // Student s2 = new Student();
+        // System.out.println("s1 " + s1);
         // s2.name = "vivek";
         // s2.roll_no = 39;
         // System.out.println(s2.name +" "+ s2.roll_no);
