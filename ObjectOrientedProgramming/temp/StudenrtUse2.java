23dd8f4a15343fb8d8a1dbcc23b29dfbe8fadc79
Deepak <deepakkumar750206@gmail.com>
2022-10-29 13:25:34 +0530

    access student in different package

--- ObjectOrientedProgramming/temp/StudenrtUse2.java
+++ ObjectOrientedProgramming/temp/StudenrtUse2.java
@@ -5,6 +5,6 @@ import ObjectOrientedProgramming.classandobjects.Student;
 public class StudenrtUse2 {
     public static void main(String[] args){
         Student s = new Student();
-        s.getRollNumber();
+        // s.getRollNumber();
     }
 }
