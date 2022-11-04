de0cb57113803525d225dcccab63dcdcd4c8682d
Deepak <deepakkumar750206@gmail.com>
2022-11-04 22:01:02 +0530

    try to understand protected modifier

--- ObjectOrientedProgramming/inheritance_temp/VehicleUse2.java
+++ ObjectOrientedProgramming/inheritance_temp/VehicleUse2.java
@@ -2,22 +2,22 @@ package ObjectOrientedProgramming.inheritance_temp;
 
 import ObjectOrientedProgramming.inheritance.Vehicle;
 
-public class VehicleUse2 {
-
+public class VehicleUse2 extends Vehicle{
+    
     public static void main(String[] args){
         Vehicle v = new Vehicle();
         // v.maxSpeed = 200; // here protected is not accessible in outside the package in derived class
         v.setColor("black");
-        v.maxSpeed = 80;
-
+        v.maxSpeed = 200;
 
         v.print();
         Truck t = new Truck();
         v.setColor("black");
-        v.maxSpeed = 80;
+        v.maxSpeed = 80; 
         t.maxLoadingCapacity = 10000;
         t.print();
 
     }
+    
     
 }
