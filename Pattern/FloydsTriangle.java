87211d94a7f06fe3ae221dd174211009df688f6c
Deepak <deepakkumar750206@gmail.com>
2022-11-12 09:58:05 +0530

    print Floyd's triangle

---
+++ Pattern/FloydsTriangle.java
@@ -0,0 +1,21 @@
+package Pattern;
+
+import java.util.Scanner;
+
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Eneter number of lines");
+        int n = sc.nextInt();
+        sc.close();
+        int num = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(num++ +" ");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
